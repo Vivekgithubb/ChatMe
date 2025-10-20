1c8d001d0b76112606ca7250e50489327e606203
D Vivek Pai <vivekpai2005810@gmail.com>
2025-10-21 00:13:51 +0530

    added deployement stuff

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
+import path from "path";
 dotenv.config({ path: "src/.env" });
 const PORT = process.env.PORT;
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
-console.log(PORT);
+//make ready for deployement
+if (process.env.NODE_ENV === "production") {
+  //getting dist from frontend to use static asset
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  //for react application
+  app.get("*", (req, res) => {
+    // res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log("Running at 3000");
 });
